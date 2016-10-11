8d5c2f944adcc1fb3c528f34d3a978ebfb8bcdb6
Kjernlie <johannes.kk@gmail.com>
2016-10-11 22:15:05 +0200

    working

--- oblig1/genetic.py
+++ oblig1/genetic.py
@@ -57,21 +57,20 @@ def mk_population(size,cities):
 # Create tournament group
 def mk_tournament(population,size):
     
-    tournament_size = population.shape[0]/size
+    number_tournaments = population.shape[0]/size
     #if (float(population.shape[0])/size).is_integer()!=True:
      #   print "Change population or tournament size!"
     #print tournament_size
         
-    tournaments = np.zeros((size,tournament_size))
+    tournaments = np.zeros((size,number_tournaments))
     population_i = np.arange(population.shape[0])
     np.random.shuffle(population_i)
     
     count = 0
     for i in range(size):
-        for j in range(tournament_size):
+        for j in range(number_tournaments):
             tournaments[i,j] = population_i[count]
             count += 1
-            
     
     return tournaments
 
@@ -80,7 +79,20 @@ def mk_tournament(population,size):
 # create parents
 # takes in the tournaments matrix from mk_tournament, the population, and which tournament you want
 def mk_parents(tournaments,population,tournament,distance_matrix):
-    
+
+
+    # create the vectors with city indices of the whole tour
+    actual_tournament = np.zeros((tournaments.shape[0],population.shape[1]))
+    for i in range(tournaments.shape[0]):
+        actual_tournament[i] = population[int(tournaments[i,int(tournament)])]
+
+    # Find the length of the actual tour
+    distances = np.zeros(tournaments.shape[0]) 
+    for i in range(tournaments.shape[0]):
+	distances[i] = dist(actual_tournament[i],distance_matrix) 
+        
+    """
+    # --------------------------------------------------
     # create the vectors with city indices of the whole tour
     actual_tournament = np.zeros((tournaments.shape[1],population.shape[1]))
     for i in range(tournaments.shape[0]): 
@@ -92,16 +104,17 @@ def mk_parents(tournaments,population,tournament,distance_matrix):
     distances = np.zeros(tournaments.shape[0]) # len actual_tour
     for i in range(tournaments.shape[0]): # len actaul_tour
         distances[i] = dist(actual_tournament[:,i],distance_matrix) # acua[i]
-
+    """
+    # Find the parents
     parents = np.argpartition(distances, 2)[:2]
-    
+ 
     global_coord_parents = np.zeros(len(parents))
     for i in range(len(global_coord_parents)):
-        global_coord_parents[i] = tournaments[parents[i],tournament]   #flipped
-    
+        global_coord_parents[i] = tournaments[parents[i],int(tournament)]   #flipped
     outcast = np.argpartition(distances,-2)[-1]
-    global_coord_outcast = tournaments[outcast,tournament]
-    
+    global_coord_outcast = tournaments[outcast,int(tournament)]
+
+
     return global_coord_parents, global_coord_outcast
 
     
@@ -121,8 +134,8 @@ def crossover(population, parents):
     find_zero1 = np.where(parent1 == 0.0)
     find_zero2 = np.where(parent2 == 0.0)
     
-    parent1[find_zero1[0]] = N+1
-    parent2[find_zero2[0]] = N+1
+    parent1[find_zero1[0]] = N
+    parent2[find_zero2[0]] = N
     
      # Creating segment size and start position
     segment_size = np.random.randint(len(parent1))  # The actual size is plus one
@@ -149,9 +162,11 @@ def crossover(population, parents):
             child[i] = parent2[i]
             
     
-    insert_zero = np.where(child == N+1)
+    insert_zero = np.where(child == N)
     child[insert_zero[0]] = 0.0
-    
+    parent1[find_zero1[0]] = 0.0
+    parent2[find_zero2[0]] = 0.0   
+ 
     return child
                     
 
@@ -189,47 +204,50 @@ def mutation(child,probability):
 def new_population(population, children, outcasts):
     new_pop = np.zeros(population.shape)
     new_pop = population.copy()
+    outcasts = np.sort(outcasts, axis = 0)
+    count = 0
     for i in range(len(outcasts)):
-        new_pop = np.delete(population, outcasts[i], 0)
+        new_pop = np.delete(new_pop, outcasts[i]-count, 0)
+        count += 1
     for i in range(len(children)):
-        #new_pop = np.vstack([new_pop,population[children[i]]])
         new_pop = np.vstack((new_pop,children[i]))
-
     return new_pop
 
 # ------------------------------------------------------------------------------------------------
 
 # main program....
 
-NoG = 100 # number of generations
-population_size = 150 # size of the population
-NoC = 22 # number of cities
+NoG = 150 # number of generations
+population_size = [50, 100, 150] # size of the population
+NoC = 24 # number of cities
 city_vec = np.arange(NoC)
 TournamentSize = 5
-NoT = population_size/TournamentSize # Number of tournaments 
 Pr = 0.03 # Probability of mutation
-population_distances = np.zeros(population_size)
-population_avg = np.zeros(NoG)
+population_avg = np.zeros((3,NoG))
 
-population = mk_population(population_size,city_vec)
-for i in range(NoG):
-    tournaments = mk_tournament(population,TournamentSize)
-    parents = np.zeros((NoT, 2))
-    outcast = np.zeros((NoT, 1))
-    child = np.zeros((NoT,len(city_vec)))
-    
-    for j in range(NoT):
-        parents[j], outcast[j] = mk_parents(tournaments,population,j,distance)
-        child[j,:] = crossover(population,parents[j])
-        child[j] = mutation(child[j],Pr)
-    population = new_population(population, child, outcast)
-    
-    for k in range(population_size):
-         population_distances[k] = dist(population[k,:],distance)
-            
-        
-    population_avg[i] = np.average(population_distances)
-        
+for m in range(len(population_size)):
+	population = mk_population(population_size[m],city_vec)
+	NoT = population_size[m]/TournamentSize # Number of tournaments 
+	population_distances = np.zeros(population_size[m])
+	for i in range(NoG):
+	    tournaments = mk_tournament(population,TournamentSize)
+	    parents = np.zeros((NoT, 2))
+	    outcast = np.zeros((NoT, 1))
+	    child = np.zeros((NoT,len(city_vec)))
+	 
+	    for j in range(NoT):
+		parents[j], outcast[j] = mk_parents(tournaments,population,j,distance)
+		child[j] = crossover(population,parents[j])
+		child[j] = mutation(child[j],Pr)
+	  
+	    population = new_population(population, child, outcast)       
+
+	    for k in range(population_size[m]):
+		population_distances[k] = dist(population[k,:],distance)
+		    
+		
+	    population_avg[m,i] = np.average(population_distances)
+		
     
 
 print "The best tour is ", np.amin(population_avg)
@@ -237,11 +255,16 @@ print "The worst tour is ", np.amax(population_avg)
 print "The mean of the tours is ", np.mean(population_avg)
 print "The standard deviaton of the tours is ", np.std(population_avg)
 
-plt.plot(population_avg)
+plt.plot(population_avg[0])
+plt.plot(population_avg[1])
+plt.plot(population_avg[2])
 plt.xlabel('Generations')
 plt.ylabel('Route length')
 plt.show()
 
+
+# ------------------------------------------------------------------------------------
+# Terminal output
 # For a population of 50 and  10 cities
 """
 The best tour is  11426.4414
